5715f2a2f21a6956df75c883600b7ce647592c09
19013345 Ju Long <a19013345@SOILABs-iMac.local>
2021-04-26 16:23:24 +0800

    lesson 2

---
+++ Lesson 2/SampleProject/SampleProject/ViewControllerA.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerA.swift
+//  SampleProject
+//
+//  Created by 19013345 Ju Long on 26/4/21.
+//
+
+import UIKit
+
+class ViewControllerA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #function, #line)
+    }
+    
+}
