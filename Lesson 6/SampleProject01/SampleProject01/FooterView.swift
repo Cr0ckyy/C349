cf235ad25aeabbbeb8de37eda2ef4aa1d16b3e03
19013345 Ju Long <a19013345@SOILABs-iMac.local>
2021-05-31 16:31:08 +0800

    commit

---
+++ Lesson 6/SampleProject01/SampleProject01/FooterView.swift
@@ -0,0 +1,13 @@
+//
+//  FooterView.swift
+//  SampleProject01
+//
+//  Created by 19013345 Ju Long on 31/5/21.
+//
+
+import UIKit
+
+class FooterView: UICollectionReusableView {
+        
+    @IBOutlet weak var outLabel: UILabel!
+}
