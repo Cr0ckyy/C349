3a7cf9bd913954a0054753425862b73e9355f51f
19013345 Ju Long <a19013345@SOILABs-iMac-si-yuan.local>
2021-07-12 15:33:07 +0800

    fix the ese exercise on lesson 7

--- Lesson 7/ESESample/ESESample/ViewController.swift
+++ Lesson 7/ESESample/ESESample/ViewController.swift
@@ -17,7 +17,9 @@ class TabelCell: UITableViewCell {
 
 class ViewController: UIViewController {
     var attractionName : String = "Singapore Zoo"
-    var address : String = "80 Mandai Lake Road, Singapore 729826"
+//    var address : String = "80 Mandai Lake Road, Singapore 729826"
+    
+    var address : String = "9 Woodlands"
 
     var overview : String = "The Singapore Zoo, formerly known as the Singapore Zoological Gardens or Mandai Zoo and now commonly known locally as the Singapore Zoo, occupies 28 hectares (69 acres) on the margins of Upper Seletar Reservoir within Singapore&apos;s heavily forested central catchment area. The zoo was built at a cost of $9 million granted by the government of Singapore and opened on 27 June 1973. It is operated by Wildlife Reserves Singapore, who also manage the neighbouring Night Safari, River Safari and the Jurong Bird Park. There are about 315 species of animal in the zoo, of which some 16 percent are considered to be threatened species. The zoo attracts 1.7 million visitors each year."
 
@@ -40,7 +42,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         HeaderLabel.text = attractionName
         AddressLabel.text = address
-        MainImageView.image = UIImage(named: "\(exhibits[Int.random(in: 0..<exhibits.count)])")
+        MainImageView.image = UIImage(named: "\(imageNames[Int.random(in: 0..<imageNames.count)])")
+        print("\(imageNames[Int.random(in: 0..<imageNames.count)])")
         MainTextView.text = overview
         
         ExhibitLabel.text = exhibitsTitle
